b8b439f99a24e50553fa42316e590300c92524d7
gaphest <gaphest66692@mail.ru>
2016-05-31 20:56:29 +0300

    last

--- admin/secure/session.inc.php
+++ admin/secure/session.inc.php
@@ -2,6 +2,6 @@
 session_start(); //мювхмюел яеяяхч
 if(!isset($_SESSION['admin'])){
     //оепедюер GET'НЛ оюпюлерп ref = рнлс юдпеяс я йнрнпнцн опхькх МЮ ЩРС ЯРПЮМХЖС
-    header("Location: /eshop/admin/secure/login.php?ref={$_SERVER['REQUEST_URI']}");
+    header("Location: /admin/secure/login.php?ref={$_SERVER['REQUEST_URI']}");
     exit;
 }
